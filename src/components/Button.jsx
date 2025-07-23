3db38888fc7ff471fff1eda742d015b9d5b9c560
Dean <dtsantos1@student.hau.edu.ph>
2025-07-23 16:48:42 +0800

    add import and add class get-started-button in Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import '../../assets/styles/button.css';
 function Button({ onClick, children }) {
   return (
-    <button onClick={onClick} style={{ margin: '0 8px', padding: '8px 12px' }}>
+    <button className='get-started-button' onClick={onClick}>
       {children}
     </button>
   );
