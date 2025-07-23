b1fbfe85aaa7f453b7ddd25f0f8c198f91e40b20
Dean <deanlt.santos@gmail.com>
2025-07-24 07:19:33 +0800

    add 2 divs to divide the hero-section content and moved cta content to the first div

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,11 +10,16 @@ function LandingPage() {
   return (
     <div className='landing-page-wrapper'>
       <section className='hero-section'>
-        <h1>📦 Welcome to Dashiiieee App</h1>
-        <p>We Deliver to your next door step.</p>
-        <Button onClick={() => navigate('/login')} className='get-started-button'>
-          Get Started
-        </Button>
+        <div>
+          <h1>📦 Welcome to Dashiiieee App</h1>
+          <p>We Deliver to your next door step.</p>
+          <Button onClick={() => navigate('/login')} className='get-started-button'>
+            Get Started
+          </Button>
+        </div>
+        <div>
+          
+        </div>
       </section>
     </div>
   );
