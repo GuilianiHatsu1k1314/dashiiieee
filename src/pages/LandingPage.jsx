a2d21c8b2705de77f30ffde8c99a0c842f1345a8
Dean <deanlt.santos@gmail.com>
2025-07-25 16:54:17 +0800

    removed box emoji on <h1> tag

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,7 +12,7 @@ function LandingPage() {
     <div className='landing-page-wrapper'>
       <section className='hero-section'>
         <div className='cta-section'>
-          <h1>📦 Your package is delivered fast to your doorstep</h1>
+          <h1>Your package is delivered fast to your doorstep</h1>
           <p>  We Deliver to your next door step. Experience fast, reliable, and convenient delivery services tailored to your needs.</p>
           <Button onClick={() => navigate('/login')} className='get-started-button'>
             <b>Get Started</b>
