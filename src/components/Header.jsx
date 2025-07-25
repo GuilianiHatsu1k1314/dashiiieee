d3c9bcb9fdcf1e7862256fb822a1efc905d26f74
Dean <deanlt.santos@gmail.com>
2025-07-25 11:35:29 +0800

    moved header element from App.jsx to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Header() {
     return (
-
+        <header style={{ position: 'fixed',padding: '10px', backgroundColor: '#f0f0f0', width: '100%' }}>
+            <h1 style={{ margin: 0, padding: '10px' }}>📦 Dashiiieee App</h1>
+        </header>
     );
 }
 
