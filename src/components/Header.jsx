dff5689fdd1e5c2a4b271632859c02c3652c5ad9
Dean <deanlt.santos@gmail.com>
2025-07-25 12:20:26 +0800

    Create nav section for header in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../assets/styles/landing-page.css';
+import Button from '../components/Button';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const navigate = useNavigate();
+    
     return (
         <header>
             <h1>📦 Dashiiieee</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/landing">Home</Link>
+                    </li>
+                    <li>
+                        <Button onClick={() => navigate('/login')}>
+                            <p>Login</p>
+                        </Button>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
