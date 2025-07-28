15f0327a16048b33efa8bcd5197335bf67ad9517
GuilPober <guillianipobre11@gmail.com>
2025-07-28 11:04:22 +0800

    Added Sign Out Feature

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
+
 function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    navigate('/');
+  };
+
   return (
     <nav className='sidebar-container'>
       <h3>Menu</h3>
@@ -9,6 +16,13 @@ function Sidebar() {
         <li><NavLink to="crud" className='sidebar-item'>CRUD</NavLink></li>
         <li><NavLink to="report" className='sidebar-item'>Report</NavLink></li>
       </ul>
+
+      {/* Sign Out Button at Bottom */}
+      <div className="sidebar-footer">
+        <button onClick={handleSignOut} className="signout-button">
+          Sign Out
+        </button>
+      </div>
     </nav>
   );
 }
