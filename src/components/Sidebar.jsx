86d47fc190248e0472b102476dbbffd472a49b54
Dean <deanlt.santos@gmail.com>
2025-07-28 08:41:34 +0800

    refactored Sidebar.jsx code

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
 function Sidebar() {
   return (
-    <div>
-      <div className='sidebar-container'>
-        <h3>📂 Menu</h3>
-        <ul>
-          <li><Link to="crud">CRUD</Link></li>
-          <li><Link to="report">Report</Link></li>
-        </ul>
-      </div>
+    <div className='sidebar-container'>
+      <h3>📂 Menu</h3>
+      <ul>
+        <li><Link to="crud">CRUD</Link></li>
+        <li><Link to="report">Report</Link></li>
+      </ul>
     </div>
   );
 }
