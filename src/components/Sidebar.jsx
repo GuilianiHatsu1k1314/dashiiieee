2fd625c26fcbe05f40b2675da4311e731af9a57e
Dean <deanlt.santos@gmail.com>
2025-07-28 09:44:26 +0800

    changed div container to nav for better readability

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import '../assets/styles/sidebar.css';
 function Sidebar() {
   return (
-    <div className='sidebar-container'>
+    <nav className='sidebar-container'>
       <h3>📂 Menu</h3>
       <ul>
         <li><Link to="crud">CRUD</Link></li>
         <li><Link to="report">Report</Link></li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
