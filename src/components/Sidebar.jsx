94ce143cb7562c9ab33813374df198426d9ca85e
Dean <deanlt.santos@gmail.com>
2025-07-28 08:24:59 +0800

    create new sidebar.css and add class called siddebar-container

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../assets/styles/sidebar.css';
 function Sidebar() {
   return (
-    <div>
+    <div className='sidebar-container'>
       <h3>📂 Menu</h3>
       <ul>
         <li><Link to="crud">CRUD</Link></li>
